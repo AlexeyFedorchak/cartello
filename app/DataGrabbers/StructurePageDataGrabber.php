2a9be5ad05e70387112a00c32b32c181cbf88b40
Oleksii Fedorchak <ofedorchak68@gmail.com>
2021-07-08 15:02:08 +0300

    Update: structure chart for pages

--- app/DataGrabbers/StructurePageDataGrabber.php
+++ app/DataGrabbers/StructurePageDataGrabber.php
@@ -7,7 +7,7 @@ use App\Charts\Models\CachedDomainList;
 use App\Charts\Models\CachedResponses;
 use App\Charts\Models\Chart;
 
-class DynamicStructureDataGrabberPages implements DataGrabber
+class StructurePageDataGrabber implements DataGrabber
 {
     /**
      * chart instance
@@ -31,24 +31,43 @@ class DynamicStructureDataGrabberPages implements DataGrabber
         $rows = [];
 
         foreach (CachedDomainList::all() as $domain) {
-            $page_1 = $this->getRow($domain->domain, 1, 10);
-            $page_2 = $this->getRow($domain->domain, 11, 20);
-            $page_3 = $this->getRow($domain->domain, 21, 30);
-            $page_4_more = $this->getRow($domain->domain, 30, PHP_INT_MAX);
+            $page_1 = collect($this->getRow($domain->domain, 1, 10))
+                ->pluck('count_clicks')
+                ->sum();
+
+            $page_2 = collect($this->getRow($domain->domain, 11, 20))
+                ->pluck('count_clicks')
+                ->sum();
+
+            $page_3 = collect($this->getRow($domain->domain, 21, 30))
+                ->pluck('count_clicks')
+                ->sum();
+
+            $page_4 = collect($this->getRow($domain->domain, 30, PHP_INT_MAX))
+                ->pluck('count_clicks')
+                ->sum();
+
+            $total = $page_1 + $page_2 + $page_3 + $page_4;
 
             $rows[$domain->domain] = [
-                'page_1' => $page_1,
-                'page_2' => $page_2,
-                'page_3' => $page_3,
-                'page_4_and_more' => $page_4_more,
+                'page_1' => round(($page_1 / $total) * 100, 1),
+                'page_2' => round(($page_2 / $total) * 100, 1),
+                'page_3' => round(($page_3 / $total) * 100, 1),
+                'page_4' => round(($page_4 / $total) * 100, 1),
+                'page_1_numeric' => $page_1,
+                'page_2_numeric' => $page_2,
+                'page_3_numeric' => $page_3,
+                'page_4_numeric' => $page_4,
             ];
 
             CachedResponses::updateOrCreate([
                 'chart_id' => $this->chart->id,
-                'domain' => $domain->domain,
+                'domain' => $domain->domain
             ], [
                 'response' => json_encode($rows[$domain->domain]),
             ]);
+
+            echo "Processed: " . $domain->domain . "\r\n";
         }
 
         return $rows;
@@ -65,7 +84,7 @@ class DynamicStructureDataGrabberPages implements DataGrabber
     private function getRow(string $domain, int $lowPosition = 1, int $highPosition = 3): array
     {
         return app(IClient::class)
-            ->select('searchanalytics', ['SUM(clicks) as count_clicks', 'SUM(impressions) as count_impressions', 'date'])
+            ->select('searchanalytics', ['SUM(clicks) as count_clicks'])
             ->where('position >= ' . $lowPosition)
             ->where('position <= ' . $highPosition)
             ->where('date <= CURRENT_DATE()')
