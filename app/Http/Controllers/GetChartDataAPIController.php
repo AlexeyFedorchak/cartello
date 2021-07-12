2f31071b1b8a4951bc24bec062d185d659a419db
Oleksii Fedorchak <ofedorchak68@gmail.com>
2021-07-12 09:56:55 +0300

    Update: added redis

--- app/Http/Controllers/GetChartDataAPIController.php
+++ app/Http/Controllers/GetChartDataAPIController.php
@@ -7,11 +7,21 @@ use App\Charts\Models\CachedResponses;
 use App\Charts\Models\Chart;
 use App\Exceptions\NoCacheFoundForGivenChart;
 use App\Http\Requests\ValidateGetChartDataRequest;
+use Illuminate\Support\Facades\Redis;
 
 class GetChartDataAPIController extends Controller
 {
     public function get(ValidateGetChartDataRequest $request)
     {
+        $redisUID = '';
+        foreach ($request->all() as $key => $value)
+            $redisUID .= $key . '-' . $value;
+
+        $redisValue = Redis::get($redisUID);
+
+        if (!empty($redisValue))
+            return $redisValue;
+
         $cachedResponses = CachedResponses::where('chart_id', $request->id)
             ->where(function ($query) use ($request) {
                 if (!empty($request->domain))
@@ -59,9 +69,13 @@ class GetChartDataAPIController extends Controller
             $data = $cachedResponses;
         }
 
-        return [
+        $response = [
             'row' => $data,
             'time_row' => $chart->time_row,
         ];
+
+        Redis::set($redisUID, json_encode($response), 'EX', 86400);
+
+        return $response;
     }
 }
