841663679c43ae7e96d44b0a0c1732b0a3ae5136
Oleksii Fedorchak <ofedorchak68@gmail.com>
2021-07-12 11:16:10 +0300

    Update: added overview

--- app/Http/Controllers/GetChartDataAPIController.php
+++ app/Http/Controllers/GetChartDataAPIController.php
@@ -49,12 +49,17 @@ class GetChartDataAPIController extends Controller
                 'previous' => [],
             ];
 
+            $currentCountImpressions = 0;
+            $prevCountImpressions = 0;
+
             foreach ($cachedResponses as $cache) {
                 foreach ($cache['current'] as $dayN => $current) {
                     if (empty($data['current'][$dayN]))
                         $data['current'][$dayN] = 0;
 
                     $data['current'][$dayN] += $current['count_clicks'];
+
+                    $currentCountImpressions += $current['count_impressions'];
                 }
 
                 foreach ($cache['previous'] as $dayN => $previous) {
@@ -62,8 +67,23 @@ class GetChartDataAPIController extends Controller
                         $data['previous'][$dayN] = 0;
 
                     $data['previous'][$dayN] += $previous['count_clicks'];
+
+                    $prevCountImpressions += $previous['count_impressions'];
                 }
             }
+
+            $totalClicksCurrent = array_sum($data['current']);
+            $totalClicksPrev = array_sum($data['previous']);
+
+            $data['overview'] = [
+                'current_count_clicks' => $totalClicksCurrent,
+                'prev_count_clicks' => $totalClicksPrev,
+                'change_clicks' => round((($totalClicksCurrent - $totalClicksPrev) / $totalClicksPrev) * 100, 2),
+
+                'current_count_impressions' => $currentCountImpressions,
+                'prev_count_impressions' => $prevCountImpressions,
+                'change_impressions' => round((($currentCountImpressions - $prevCountImpressions) / $prevCountImpressions) * 100, 2),
+            ];
         } else {
             $data = $cachedResponses;
         }
