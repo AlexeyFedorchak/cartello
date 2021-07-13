7866e66d1ec6ae58920103ae720fd93f71f7bf58
Oleksii Fedorchak <ofedorchak68@gmail.com>
2021-07-13 09:31:20 +0300

    Fix: added avg position chart + fixes for opportunity table - v2

--- app/Charts/Console/Commands/CacheChartResponses.php
+++ app/Charts/Console/Commands/CacheChartResponses.php
@@ -31,9 +31,6 @@ class CacheChartResponses extends Command
         shell_exec('export GOOGLE_APPLICATION_CREDENTIALS="/home/forge/edgytech.space/normandy-api-d4370f73211d.json"');
 
         foreach (Chart::all() as $chart) {
-            if ($chart->id !== 81)
-                continue;
-
             $this->info('Processing chart: ' . $chart->id);
 
             try {
