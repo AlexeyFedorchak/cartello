6eda8e77a9344a23f1501a00c4962cb23c0ef9fe
Oleksii Fedorchak <ofedorchak68@gmail.com>
2021-07-29 11:51:47 +0300

    Fix: fixed domain list to make compatible with front-end structure

--- app/Http/Controllers/ConsoleAPI/FilterOpportunityChartAPIController.php
+++ app/Http/Controllers/ConsoleAPI/FilterOpportunityChartAPIController.php
@@ -9,13 +9,15 @@ class FilterOpportunityChartAPIController extends ConsoleAPIController
 {
     public function filter(ValidateFilterOpportunityChartRequest $request)
     {
+        $domains = explode(',', str_replace(['[', ']', '"', '"'], '', ($request->domains[0])));
+
         $chart = Chart::where($request->only('id'))
             ->first();
 
         return $chart->getFilter()->filterAndSort(
             $request->page,
             $request->filters,
-            $request->domains,
+            $domains,
             !empty($request->sortBy) ? $request->sortBy : 'opportunities',
             !empty($request->direction) ? $request->direction : 'desc',
         );
